6554d2d2740eae78f7cca22eee5e64c09a07a654
Snovia2398 <snovia2398@gmail.com>
2021-04-12 02:16:52 +0500

    Navigation Drawer

---
+++ Lecture12/app/src/main/java/com/snovia/navigation_practice/ChromeReaderActivity.java
@@ -0,0 +1,14 @@
+package com.snovia.navigation_practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChromeReaderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chrome_reader);
+    }
+}
---
+++ Lecture12/app/src/main/java/com/snovia/navigation_practice/InsertCommentActivity.java
@@ -0,0 +1,14 @@
+package com.snovia.navigation_practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InsertCommentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_comment);
+    }
+}
